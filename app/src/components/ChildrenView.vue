a75c218a5fa86ab09c16c5b4b4a731961e328e46
gty040627 <3169028263@qq.com>
2023-06-29 18:39:49 +0800

    day03

---
+++ app/src/components/ChildrenView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!-- 匿名插槽 -->
+    <slot></slot>
+    <!-- 具名插槽 -->
+    <slot name="gty"></slot>
+    <!-- 作用域插槽 -->
+    <slot :games="games" name="item"></slot>      
+ </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+        games:['王者荣耀','绝地求生','连连看']
+    }
+  },
+  methods: {
+
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  components: {
+
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
