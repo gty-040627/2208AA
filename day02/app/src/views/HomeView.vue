bb69ee60d22105177d54ca471cd8e76f321868c6
gty040627 <3169028263@qq.com>
2023-06-29 18:58:11 +0800

    day03

--- day02/app/src/views/HomeView.vue
+++ day02/app/src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div>
+    高甜媛
+ </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
   components: {
-    HelloWorld
-  }
+
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
 }
 </script>
+
+<style lang='scss' scoped>
+</style>
+
